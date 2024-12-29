a6c6d4e40dd653dcbb5d0e429b3f1f9fffbafb57
Faqeha Noor <162030362+faqehanoor@users.noreply.github.com>
2024-12-29 17:28:44 +0500

    Update Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-export default function() {
+export default function ContactUs() {
   return (
-    <div>
-      <h1 className='text-5xl text-center m-10'>𝕮𝖔𝖓𝖙𝖆𝖈𝖙 𝖀𝖘 𝕹𝖔𝖜!</h1>
-      <ul className='text-center text-2xl text-gray-400'>
-        <li>𝓔𝓶𝓪𝓲𝓵: <span>faqehanoor022@gmail.com</span></li>
-        <li>𝓟𝓱𝓸𝓷𝓮: <span>0330254356</span></li>
-        <li>𝓛𝓲𝓷𝓴𝓮𝓭𝓘𝓷: Faqeha Noor</li>
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <h1 className="text-5xl text-center m-10">
+        𝕮𝖔𝖓𝖙𝖆𝖈𝖙 𝖀𝖘 𝕹𝖔𝖜!
+      </h1>
+      <ul className="text-center text-2xl text-gray-600 space-y-4">
+        <li>
+          <span className="font-semibold">Email:</span> faqehanoor022@gmail.com
+        </li>
+        <li>
+          <span className="font-semibold">Phone:</span> 0330254356
+        </li>
+        <li>
+          <span className="font-semibold">LinkedIn:</span> <a href="https://www.linkedin.com/in/faqeha-noor-2b5a4a254/" className="text-blue-500 hover:underline">Faqeha Noor</a>
+        </li>
       </ul>
-     
     </div>
-  )
+  );
 }
