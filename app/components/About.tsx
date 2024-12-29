833c4ca880a2dffe3337527c1c891570cc2d58f0
Faqeha Noor <162030362+faqehanoor@users.noreply.github.com>
2024-12-29 17:18:03 +0500

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,16 +1,12 @@
-import Image from 'next/image';
-import React from 'react';
-
-
 export default function AboutUs() {
   return (
-    <div>
-     
-      <h1 className='text-center text-5xl m-10'>𝔸𝕓𝕠𝕦𝕥 𝕌𝕤</h1>
-      <p className='text-gray-400 text-center'>
-      Pakistan's Eat is a culinary journey that celebrates the rich and diverse flavors of Pakistani cuisine.
-      <br /> Our mission is to bring the authentic taste of Pakistan to your kitchen, offering recipes that range from traditional classics to modern twists.
-       <br></br> Thank you for visiting . We look forward to sharing the joy of cooking with you!
+    <div className="flex flex-col items-center">
+      <h1 className="text-center text-5xl m-10">𝔸𝕓𝕠𝕦𝕥 𝕌𝕤</h1>
+      
+      <p className="text-gray-400 text-center mt-6 mx-4">
+        Pakistan&apos;s Eat is a culinary journey that celebrates the rich and diverse flavors of Pakistani cuisine.
+        <br /> Our mission is to bring the authentic taste of Pakistan to your kitchen, offering recipes that range from traditional classics to modern twists.
+        <br /> Thank you for visiting. We look forward to sharing the joy of cooking with you!
       </p>
     </div>
   );
